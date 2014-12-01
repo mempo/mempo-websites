eb70c82c635a78dcdc13d7f50bf4eb00d22268d3
mempoweb <ferohnae@wp.pl>
2014-12-01 15:12:25 +0100

    get random litecoin: RPC pass in local file
    it was anyway just for local connections and not for normal
    serious use, but eitherway lets default to proper password
    configuration in case anyone else would use this script

---
+++ mempo-main/html/random/blockchain/litecoin/get/password.EXAMPLE.php
@@ -0,0 +1,7 @@
+<?php
+
+// copy this file to same name but 'LOCAL' (in uppercase) instead 'EXAMPLE'
+
+// write here your coin server RPC password that you will configure in the coin server
+$rpcpassword='yourpassword';
+
